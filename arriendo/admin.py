3942592a53807aff98c3927fb69996df8628ca43
franciscoalejandro898 <129436140+franciscoalejandro898@users.noreply.github.com>
2023-10-12 22:33:35 -0300

    modelos y migraciones exitosas

--- arriendo/admin.py
+++ arriendo/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import *
+# configuracion de django admin
 
-# Register your models here.
+
+class CategoriasAdmin(admin.ModelAdmin):
+    list_display = ('id_cat', 'nombre_cat')
+    search_fields = ['nombre_cat']
+    
+    
+class BodegaAdmin(admin.ModelAdmin):
+    list_display = ('id_bodega', 'nombre_bod')
+    search_fields = ['nombre_bod']
+    
+    
+    
+class MaquinariaAdmin(admin.ModelAdmin):
+    list_display = ('id_m', 'nombre_m', 'categoria_m', 'bodega_m')
+    
+    
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('id_cliente', 'nombre', 'apellido', 'rut', 'telefono')
+    
+    
+class ArriendosAdmin(admin.ModelAdmin):
+    list_display = ('id_arriendo', 'maquina_arriendo', 'cliente_arriendo', 'bodega_arriendo','fecha_arriendo')
+    
+class EstadoMaquinariaAdmin(admin.ModelAdmin):
+    list_display = ('id_estado', 'nombre_est_maquina', 'estado')
+    
+
+admin.site.register(Categorias, CategoriasAdmin)
+admin.site.register(Bodegas,BodegaAdmin)
+admin.site.register(Maquinaria,MaquinariaAdmin)
+admin.site.register(Cliente,ClienteAdmin)
+admin.site.register(Arriendos,ArriendosAdmin)
+admin.site.register(EstadoMaquinaria,EstadoMaquinariaAdmin)
--- arriendo/models.py
+++ arriendo/models.py
@@ -1,3 +1,94 @@
 from django.db import models
 
+# definicion de los modelos
+
+
+from django.db import models
+
 # Create your models here.
+#Tabla Categorias
+class Categorias(models.Model):
+   id_cat = models.AutoField('Id Categoria',primary_key=True)
+   nombre_cat = models.CharField('Tipo de Categoria',max_length=20)
+   
+   def categorias(self):
+       return "{} {}".format(self.id_cat,self.nombre_cat)
+   
+   def __str__(self):
+       return self.categorias()
+   
+    #Tabla Bodega  
+class Bodegas(models.Model):
+    id_bodega = models.AutoField('Id Bodega', primary_key=True)
+    nombre_bod = models.CharField('Nombre Bodega', max_length=30)
+    
+    def bodegas(self):
+       return "{} {}".format(self.id_bodega,self.nombre_bod)
+    
+    def __str__(self):
+       return self.bodegas()
+
+    #Tabla Maquinaria
+
+class Maquinaria(models.Model):
+    ESTADO_CHOICES = [
+        ('disponible', 'Disponible'),
+        ('no_disponible', 'No Disponible'),
+    ]
+    
+    id_m = models.AutoField(primary_key=True)
+    sku = models.CharField(max_length=20)
+    nombre_m = models.CharField(max_length=20)
+    categoria_m = models.ForeignKey(Categorias, on_delete=models.SET_NULL, null=True)
+    bodega_m = models.ForeignKey(Bodegas, on_delete=models.SET_NULL, null=True)
+    estado = models.CharField(max_length=20, choices=ESTADO_CHOICES, default='disponible')
+
+   
+    def maquinaria(self):
+       return "{} {} {} {} {}".format(self.id_m,self.nombre_m, self.categoria_m, self.bodega_m, self.estado)
+   
+    def __str__(self):
+       return self.maquinaria()
+   
+    #Tabla Cliente
+class Cliente(models.Model):
+   id_cliente = models.AutoField(primary_key=True)
+   nombre = models.CharField( max_length=20)
+   apellido = models.CharField( max_length=20)
+   rut = models.CharField(max_length=40)
+   telefono = models.CharField( max_length=20)  
+   
+   def cliente(self):
+       return "{} {} {} {} {}".format(self.id_cliente,self.nombre,self.apellido, self.rut,self.telefono)
+  
+   
+   def __str__(self):
+       return self.cliente()
+   
+    #Tabla Arriendos 
+class Arriendos(models.Model): 
+    id_arriendo = models.AutoField(primary_key=True)
+    maquina_arriendo = models.ForeignKey(Maquinaria, on_delete=models.SET_NULL,null=True)
+    cliente_arriendo = models.ForeignKey(Cliente, on_delete=models.SET_NULL,null=True)
+    bodega_arriendo = models.ForeignKey(Bodegas, on_delete=models.SET_NULL,null=True)
+    fecha_arriendo = models.DateField(null=False, blank=False)
+    
+    def arriendo(self):
+       return "{} {} {} {} {}".format(self.id_arriendo,self.maquina_arriendo, self.cliente_arriendo, self.bodega_arriendo,self.fecha_arriendo)
+    
+    def __str__(self):
+       return self.arriendo()
+   
+   #Tabla EstadoMaquinaria
+class EstadoMaquinaria(models.Model):
+    id_estado = models.AutoField(primary_key=True)
+    nombre_est_maquina = models.ForeignKey(Maquinaria,on_delete=models.SET_NULL,null=True)
+    l_estado = (('Y', 'Disponible'), ('N', 'No Disponible'))
+    estado = models.CharField(max_length=1,choices=l_estado,default='Y')
+    
+    
+    def estado_maquinaria(self):
+       return "{} {} {}".format(self.id_estado,self.nombre_est_maquina,self.estado)
+    
+    def __str__(self):
+       return self.estado_maquinaria()
